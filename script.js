3bd716d4947028d370f332e96134e90e26978cee
Sunidh <99424874+sunidh1908@users.noreply.github.com>
2023-02-15 17:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+var btn = document.getElementById("btn");
+var pnum = document.getElementById("num");
+var res = document.getElementById("respond");
+var inputValue = document.getElementById("guess").value;
+
+function actionPerformed() {
+	var nums = Math.floor(Math.random() * 40) - 20;
+	document.getElementById("num").innerText = nums;
+	guessNum();
+	print();
+}
+
+function print() {
+
+	if(Math.abs(nums - input) <= 5){
+		document.getElementById("respond").innerText = "Hot"; 
+	}
+	else{
+		document.getElementById("respond").innerText = "Cold"; 
+	}
+	
+}
+
+function guessNum(){
+	input = Number(document.getElementById("guess").value);
+}
+
+btn.addEventListener("click",actionPerformed);
+
